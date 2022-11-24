d653ee057ca95e2f9a57eb6c8ad5b19ba4d6156b
Luis Barroso <luiisgbl@gmail.com>
2022-11-23 22:31:02 -0300

    version mejorada

--- js/swiper.js
+++ js/swiper.js
@@ -13,30 +13,19 @@ var swiper = new Swiper('.swiper-container', {
 		spaceBetween: 20,
 	  },
 	  680: {
-<<<<<<< HEAD
 		slidesPerView: 1,
 		spaceBetween: 40,
 	  },
 	  780: {
-=======
->>>>>>> main
 		slidesPerView: 2,
 		spaceBetween: 40,
 	  },
 	  920: {
-<<<<<<< HEAD
 		slidesPerView: 3,
 		spaceBetween: 40,
 	  },
 	  1240: {
 		slidesPerView: 4,
-=======
-		slidesPerView: 2,
-		spaceBetween: 40,
-	  },
-	  1240: {
-		slidesPerView: 3,
->>>>>>> main
 		spaceBetween: 50,
 	  },
 	} 
